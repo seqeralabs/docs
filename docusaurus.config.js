a93dfe3012966acedf20c904ebc47f722d0035ce
llewellyn-sl <llewelyn-van-der-berg@seqera.io>
2024-06-19 19:26:39 +0200

    Add MultiQC submodule

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ export default async function createConfigAsync() {
         {
           id: "multiqc",
           routeBasePath: "/multiqc",
-          path: "multiqc_docs",
+          path: "multiqc_docs/multiqc_repo/docs",
           remarkPlugins: [
             (await import("remark-code-import")).default,
             (await require("remark-math")).default,
