0d0676a09a97ad9d2eb090cb953b969a56391a38
Phil Ewels <phil.ewels@seqera.io>
2024-10-04 00:39:01 +0200

    Blog tags next to the date, colour by product

--- src/theme/BlogPostItem/Header/Info/index.js
+++ src/theme/BlogPostItem/Header/Info/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import clsx from 'clsx';
 import {useDateTimeFormat} from '@docusaurus/theme-common/internal';
 import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
+import Link from '@docusaurus/Link';
+
 import styles from './styles.module.css';
 
 function DateTime({date, formattedDate}) {
@@ -9,17 +11,23 @@ function DateTime({date, formattedDate}) {
 }
 export default function BlogPostItemHeaderInfo({className}) {
   const {metadata} = useBlogPost();
-  const {date} = metadata;
+  const {date, tags} = metadata;
   const dateTimeFormat = useDateTimeFormat({
     day: 'numeric',
     month: 'long',
     year: 'numeric',
     timeZone: 'UTC',
   });
+  const tagsExists = tags.length > 0;
   const formatDate = (blogDate) => dateTimeFormat.format(new Date(blogDate));
   return (
-    <div className={clsx(styles.container, 'margin-vert--md', className)}>
+    <div className={styles.container}>
       <DateTime date={date} formattedDate={formatDate(date)} />
+      {tagsExists && tags.map((tag) => (
+        <div className={clsx(styles.tag, styles[tag.label.replace(' ', '-')])} key={tag.permalink}>
+          <Link to={tag.permalink}>{tag.label}</Link>
+        </div>
+      ))}
     </div>
   );
 }
