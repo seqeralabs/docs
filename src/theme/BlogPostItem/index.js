c8853ebdc06931cbbb40a38e2a83ba6af49a480d
Justine Geffen <justinegeffen@users.noreply.github.com>
2024-10-03 20:50:14 +0200

    Fixed blog layout file

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -6,18 +6,10 @@ export default function BlogLayoutWrapper(props) {
   return (
     <>
       <BlogLayout {...props} />
-      <div style={{ marginTop: '2rem' }}>
-        <Link to="/docs/">Back to Docs</Link>
-      </div>
-    </>
-  );
-}
-
-export default function BlogLayoutWrapper(props) {
-  return (
-    <>
-      <BlogLayout {...props} />
       <div style={{ marginTop: '2rem', textAlign: 'center' }}>
+        <Link to="/docs/">Back to Docs</Link> {/* "Back to Docs" link */}
+      </div>
+      <div style={{ marginTop: '1rem', textAlign: 'center' }}>
         <a href="/blog/rss.xml" target="_blank" rel="noopener noreferrer">
           <button style={{ padding: '0.5rem 1rem', fontSize: '16px' }}>Subscribe to RSS</button>
         </a>
@@ -25,3 +17,4 @@ export default function BlogLayoutWrapper(props) {
     </>
   );
 }
+
