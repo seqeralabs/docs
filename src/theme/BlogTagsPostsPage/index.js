7622e09510564562559f67715ccd58eb2d4906b6
Phil Ewels <phil.ewels@seqera.io>
2024-10-04 09:13:19 +0200

    Changelog page titles, improve product/tag list page

---
+++ src/theme/BlogTagsPostsPage/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import {
+  PageMetadata,
+  HtmlClassNameProvider,
+  ThemeClassNames,
+} from '@docusaurus/theme-common';
+import {useBlogTagsPostsPageTitle} from '@docusaurus/theme-common/internal';
+import Link from '@docusaurus/Link';
+import BlogLayout from '@theme/BlogLayout';
+import BlogListPaginator from '@theme/BlogListPaginator';
+import SearchMetadata from '@theme/SearchMetadata';
+import BlogPostItems from '@theme/BlogPostItems';
+import Unlisted from '@theme/ContentVisibility/Unlisted';
+import Heading from '@theme/Heading';
+function BlogTagsPostsPageMetadata({tag}) {
+  const title = useBlogTagsPostsPageTitle(tag);
+  return (
+    <>
+      <PageMetadata title={title} description={tag.description} />
+      <SearchMetadata tag="blog_tags_posts" />
+    </>
+  );
+}
+function BlogTagsPostsPageContent({tag, items, sidebar, listMetadata}) {
+  console.log('tag', tag);
+  return (
+    <BlogLayout sidebar={sidebar}>
+      {tag.unlisted && <Unlisted />}
+      <header className="margin-bottom--xl">
+        <Heading as="h1" style={{ textTransform: 'capitalize', fontSize: '3em' }}>Changelog: {tag.label}</Heading>
+        {tag.description && <p>{tag.description}</p>}
+      </header>
+      <BlogPostItems items={items} isTagPage={true} />
+      <BlogListPaginator metadata={listMetadata} />
+    </BlogLayout>
+  );
+}
+export default function BlogTagsPostsPage(props) {
+  return (
+    <HtmlClassNameProvider
+      className={clsx(
+        ThemeClassNames.wrapper.blogPages,
+        ThemeClassNames.page.blogTagPostListPage,
+      )}>
+      <BlogTagsPostsPageMetadata {...props} />
+      <BlogTagsPostsPageContent {...props} />
+    </HtmlClassNameProvider>
+  );
+}
