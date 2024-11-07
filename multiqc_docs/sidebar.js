90e70c01a288bc6eae82cb94f5a65e6e9c213863
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2024-11-07 18:25:13 +0100

    Add changelog

--- multiqc_docs/sidebar.js
+++ multiqc_docs/sidebar.js
@@ -65,9 +65,9 @@ export default {
         "items": []
       },
       {
-        "type": "link",
+        "type": "doc",
         "label": "Changelog",
-        "href": "/changelog/tags/multiqc"
+        "id": "changelog"
       }
     ]
   ]
