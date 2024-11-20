6a348f419a6da3d30aa1c9392620329c8b5f3572
Vlad Savelyev <vladislav.sav@gmail.com>
2024-11-20 20:39:31 +0100

    Remove MultiQC as a submodule and instead clone as prebuild (#298)
    * Remove MultiQC as a submodule and instead clone as prebuild
    
    * Wave too
    
    * Trailing comma
    
    * Check .git folder
    
    * Remove predev
    
    * Update packages
    
    * Gitignore
    
    * Remove wave submodule
    
    * Try-catch

---
+++ internal/fetch-docs-oss.mjs
@@ -0,0 +1,62 @@
+import git from "simple-git";
+import fs from "fs-extra";
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const repositories = [
+  {
+    name: 'multiqc',
+    url: 'https://github.com/MultiQC/MultiQC.git',
+    path: path.join(__dirname, "..", "multiqc_docs", "multiqc_repo")
+  },
+  {
+    name: 'wave',
+    url: 'https://github.com/seqeralabs/wave.git',
+    path: path.join(__dirname, "..", "wave_docs", "wave_repo")
+  }
+];
+
+// Function to clone or update a repository
+async function cloneOrUpdateRepo({ name, url, path: repoPath }) {
+  const exists = await fs.pathExists(repoPath + "/.git");
+  
+  try {
+    if (exists) {
+      console.log(`${name}: Cloned repo already exists, will pull latest changes`);
+      await git(repoPath).pull();
+      console.log(`✅ ${name}: Pulled latest changes`);
+    } else {
+      console.log(`${name}: Cloning into ${repoPath}`);
+      await git().clone(url, repoPath);
+      console.log(`✅ ${name}: Cloned`);
+    }
+  } catch (error) {
+    console.error(`❌ Error processing ${repo.name}:`);
+    console.error(error.message);
+    if (exists) {
+      console.error(`Try removing the directory: ${repoPath}`);
+    }
+    // Exit with error code 1 to stop any subsequent npm scripts
+    process.exit(1);
+  }
+
+  return git(repoPath);
+}
+
+// Clone/update all repositories
+async function processRepositories() {
+  for (const repo of repositories) {
+    try {
+      await cloneOrUpdateRepo(repo);
+    } catch (error) {
+      console.error(`❌ Error processing ${repo.name}:`, error);
+    }
+  }
+}
+
+// Execute
+processRepositories().catch(console.error);
