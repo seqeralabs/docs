b8ae3e4bd08e1bbde50090124317caa9222446eb
Justine Geffen <justinegeffen@users.noreply.github.com>
2024-11-25 09:55:27 +0200

    404 content

--- src/theme/NotFound/index.js
+++ src/theme/NotFound/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import NotFound from '@theme-original/NotFound';
+import { Link } from '@docusaurus/Link';
 
-export default function NotFoundWrapper(props) {
-  return (
-    <>
-      <NotFound {...props} />
-    </>
+function NotFound() {
+  return React.createElement(
+    'div',
+    { style: { textAlign: 'center', padding: '50px' } },
+    React.createElement('h1', null, 'Page not found'),
+    React.createElement('p', null, 'Sorry, we couldn\'t find the page you\'re looking for.'),
+    React.createElement(
+      'p',
+      null,
+      'You can return to the ',
+      React.createElement(Link, { to: '/' }, 'home page'),
+      ' or check out our ',
+      React.createElement(Link, { to: '/docs' }, 'documentation'),
+      ' for more information.'
+    )
   );
 }
+
+export default NotFound;
