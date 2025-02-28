4ec3f657afef89d2f228887ba38a30ff178b9c2e
Llewellyn vd Berg <113503285+llewellyn-sl@users.noreply.github.com>
2025-02-28 13:14:47 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,8 +83,8 @@ export default async function createConfigAsync() {
       [
         "@docusaurus/plugin-content-docs",
         {
-          id: "platform-enterprise",
-          routeBasePath: "/platform-enterprise",
+          id: "platform",
+          routeBasePath: "/platform",
           includeCurrentVersion: false,
           remarkPlugins: [
             (await import("remark-code-import")).default,
