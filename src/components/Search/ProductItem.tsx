dbfd4dfb83462df73f85c02f53c65e752b6346d4
Deep Shah <deepcshah@gmail.com>
2025-04-29 17:19:20 -0400

    fixed cloud vs enterprise tags designation

--- src/components/Search/ProductItem.tsx
+++ src/components/Search/ProductItem.tsx
@@ -32,19 +32,27 @@ export default function ProductItem({ hit, components }) {
           </div>
         </div>
         <div className="flex-shrink-0 text-right">
-          {(hit.url.includes('https://docs.seqera.io/platform') || hit.url.includes('https://docs.seqera.io/changelog/page') || hit.url.includes('https://docs.seqera.io/changelog/tags/seqera-enterprise')) && (
+          {(hit.url.includes('https://docs.seqera.io/platform-enterprise') || hit.url.includes('https://docs.seqera.io/changelog/page') || hit.url.includes('https://docs.seqera.io/changelog/seqera-enterprise/')) && (
             <div className="flex items-start pb-2 last:pb-0">
-              <span className="text-xs mr-2 px-2 py-1 bg-product-100 rounded-md text-product-1000">Seqera Platform</span>
+              <span className="text-xs mr-2 px-2 py-1 bg-product-100 rounded-md text-product-1000">Seqera Enterprise</span>
               {hit._highlightResult?.version?.[0]?.value && (
                 <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap"><components.Highlight hit={hit._highlightResult.version[0]} attribute="value" tagName="b" className="whitespace-pre-wrap" /></span>
               )}
             </div>
           )}
-          {(hit.url.match(/https:\/\/docs\.seqera\.io\/([^\/]+)/)?.[1] === 'wave' || hit.url.includes('https://docs.seqera.io/changelog/tags/wave') || hit.url.includes('https://docs.seqera.io/changelog/wave')) && (
+          {(hit.url.includes('https://docs.seqera.io/platform-cloud') || hit.url.includes('https://docs.seqera.io/changelog/seqera-cloud/')) && (
+            <div className="flex items-start pb-2 last:pb-0">
+              <span className="text-xs mr-2 px-2 py-1 bg-product-100 rounded-md text-product-1000">Seqera Cloud</span>
+              {hit._highlightResult?.version?.[0]?.value && (
+                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap">Latest</span>
+              )}
+            </div>
+          )}
+          {(hit.url.includes('https://docs.seqera.io/wave') || hit.url.match(/https:\/\/docs\.seqera\.io\/([^\/]+)/)?.[1] === 'wave' || hit.url.includes('https://docs.seqera.io/changelog/tags/wave') || hit.url.includes('https://docs.seqera.io/changelog/wave')) && (
             <div className="flex items-start pb-2 last:pb-0">
               <span className="text-xs mr-2 px-2 py-1 bg-wave-100 rounded-md text-wave-1000">Wave</span>
               {hit._highlightResult?.version?.[0]?.value && (
-                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap"><components.Highlight hit={hit._highlightResult.version[0]} attribute="value" tagName="b" className="whitespace-pre-wrap" /></span>
+                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap">Latest</span>
               )}
             </div>
           )}
@@ -52,7 +60,7 @@ export default function ProductItem({ hit, components }) {
             <div className="flex items-start pb-2 last:pb-0">
               <span className="text-xs mr-2 px-2 py-1 bg-multiqc-100 rounded-md text-multiqc-1000">MultiQC</span>
               {hit._highlightResult?.version?.[0]?.value && (
-                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap"><components.Highlight hit={hit._highlightResult.version[0]} attribute="value" tagName="b" className="whitespace-pre-wrap" /></span>
+                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap">Latest</span>
               )}
             </div>
           )}
@@ -60,7 +68,7 @@ export default function ProductItem({ hit, components }) {
             <div className="flex items-start pb-2 last:pb-0">
               <span className="text-xs mr-2 px-2 py-1 bg-fusion-100 rounded-md text-fusion-1000">Fusion</span>
               {hit._highlightResult?.version?.[0]?.value && (
-                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap"><components.Highlight hit={hit._highlightResult.version[0]} attribute="value" tagName="b" className="whitespace-pre-wrap" /></span>
+                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap">Latest</span>
               )}
             </div>
           )}
@@ -68,7 +76,7 @@ export default function ProductItem({ hit, components }) {
             <div className="flex items-start pb-2 last:pb-0">
               <span className="text-xs mr-2 px-2 py-1 bg-nextflow-100 rounded-md text-nextflow-1000">Nextflow</span>
               {hit._highlightResult?.version?.[0]?.value && (
-                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap"><components.Highlight hit={hit._highlightResult.version[0]} attribute="value" tagName="b" className="whitespace-pre-wrap" /></span>
+                <span className="text-xs px-2 py-1 bg-slate-100 rounded-md text-slate-1000 whitespace-pre-wrap">Latest</span>
               )}
             </div>
           )}
