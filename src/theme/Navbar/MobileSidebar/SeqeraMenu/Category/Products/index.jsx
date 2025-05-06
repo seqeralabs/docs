9ac770a0711a5bbe4889fac29ea3bcb7d45cfb25
dana-seqera <dana.kim@seqera.io>
2025-05-06 16:52:23 -0500

    remove class

--- src/theme/Navbar/MobileSidebar/SeqeraMenu/Category/Products/index.jsx
+++ src/theme/Navbar/MobileSidebar/SeqeraMenu/Category/Products/index.jsx
@@ -45,14 +45,14 @@ const Products = ({ toggleMenu }) => {
       <li>
         <a href="/https://training.nextflow.io/latest/" onClick={toggleMenu}>
           Nextflow Training 
-          <div className="opacity-60 ml-1"><svg  width="12" height="12" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+          <div className="opacity-60 ml-1"><svg  width="12" height="12" aria-hidden="true" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
           </div>
         </a>
       </li>
       <li>
         <a href="/platform-api" onClick={toggleMenu}>
           Platform API  
-          <div className="opacity-60 ml-1"><svg  width="12" height="12" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+          <div className="opacity-60 ml-1"><svg  width="12" height="12" aria-hidden="true" viewBox="0 0 24 24"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
           </div>
         </a>
       </li>
