66150416454d11d0590d4fd190db5b5837fdb3f9
dana-seqera <dana.kim@seqera.io>
2025-05-09 18:08:38 -0500

    remove mobile version dropdown

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -171,11 +171,11 @@ export default async function createConfigAsync() {
             position: 'left',
             value: '<a href="https://docs.seqera.io/platform-enterprise" class="menu__link">Platform Enterprise</a>'
           },
-          {
-            type: "docsVersionDropdown",
-            position: "right",
-            docsPluginId: "platform-enterprise",
-          },
+          // {
+          //   type: "docsVersionDropdown",
+          //   position: "right",
+          //   docsPluginId: "platform-enterprise",
+          // },
           {
             to: "https://www.nextflow.io/docs/latest/",
             html: 'Nextflow <svg width="12" height="12" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU" style="margin-left:6px;opacity:0.6;"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>',
