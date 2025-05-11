7fc490d5e98d366fe3e65f7cc9046fb920cb559b
dana-seqera <dana.kim@seqera.io>
2025-05-11 13:04:52 -0500

    add versioned docs redirects

---
+++ src/pages/platform-enterprise/24.1/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function RedirectPage() {
+  React.useEffect(() => {
+    window.location.href = "https://deploy-preview-604--seqera-docs.netlify.app/platform-enterprise/24.1";
+  }, []);
+  return null;
+}
