e9fa2fc8361b6d1bd24f11bd2861cf47c77559a5
dana-seqera <dana.kim@seqera.io>
2025-05-11 13:45:20 -0500

    add platform links

--- src/theme/Navbar/Layout/SeqeraHeader/HeaderDesktop/NavItems/index.jsx
+++ src/theme/Navbar/Layout/SeqeraHeader/HeaderDesktop/NavItems/index.jsx
@@ -89,11 +89,11 @@ const NavItems = ({ isDark = false, hideMenu }) => {
               </div>
             </a>
           </li>
-          {/* <li>
-            <ActiveLink url="/platform-api" customClasses={`ml-8`}>
+          <li>
+            <ActiveLink url="/platform-api/seqera-api" customClasses={`ml-8`}>
               Platform API
             </ActiveLink>
-          </li> */}
+          </li>
         </ul>
       </div>
     </div>
