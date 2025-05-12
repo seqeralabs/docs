ba0ea79742569e1f5a116d7ba199e3f737de7684
dana-seqera <dana.kim@seqera.io>
2025-05-12 11:33:15 -0500

    cleanup: remove unused jobs and events

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -265,8 +265,6 @@ export default async function createConfigAsync() {
           },
         };
       },
-      // path.resolve(__dirname, "plugins_custom/seqera_jobs"),
-      // path.resolve(__dirname, "plugins_custom/seqera_events"),
     ],
 
     themeConfig: {
