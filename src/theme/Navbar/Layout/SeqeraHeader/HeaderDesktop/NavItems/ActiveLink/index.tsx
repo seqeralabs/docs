ff436b062e4450707467166601e438bfb7ea8f29
dana-seqera <dana.kim@seqera.io>
2025-05-23 12:39:48 -0500

    fix: active link platform-api

--- src/theme/Navbar/Layout/SeqeraHeader/HeaderDesktop/NavItems/ActiveLink/index.tsx
+++ src/theme/Navbar/Layout/SeqeraHeader/HeaderDesktop/NavItems/ActiveLink/index.tsx
@@ -4,17 +4,25 @@ import styles from "./activelink.module.css";
 
 const ActiveLink = ({ url, externalLink, exact, children, customClasses, ...rest }) => {
   
+  const currentPath = window.location.pathname;
+  const getFirstSegment = (path) => path.split("/").filter(Boolean)[0] || "";
+
+  const currentFirst = getFirstSegment(currentPath);
+  const urlFirst = getFirstSegment(new URL(url, window.location.origin).pathname);
+
+  const isActive = currentFirst === urlFirst;
+
   if (externalLink) {
     return (
       <a
         href={url}
-        className={`${styles.navLink} ${customClasses}`}
+        className={`${styles.navLink} ${customClasses} ${isActive ? styles.isActive : ''}`}
+        {...rest}
       >
         {children}
       </a>
     );
   }
-
     return (
       <NavLink
         to={url}
