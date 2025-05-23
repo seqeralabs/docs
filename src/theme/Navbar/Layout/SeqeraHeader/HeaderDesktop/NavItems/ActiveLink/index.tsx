80f066d2b0dcd7d037e6db9f0e1851a194af8389
dana-seqera <dana.kim@seqera.io>
2025-05-23 12:55:32 -0500

    Revert "fix: active link platform-api"
    This reverts commit ff436b062e4450707467166601e438bfb7ea8f29.

--- src/theme/Navbar/Layout/SeqeraHeader/HeaderDesktop/NavItems/ActiveLink/index.tsx
+++ src/theme/Navbar/Layout/SeqeraHeader/HeaderDesktop/NavItems/ActiveLink/index.tsx
@@ -4,25 +4,17 @@ import styles from "./activelink.module.css";
 
 const ActiveLink = ({ url, externalLink, exact, children, customClasses, ...rest }) => {
   
-  const currentPath = window.location.pathname;
-  const getFirstSegment = (path) => path.split("/").filter(Boolean)[0] || "";
-
-  const currentFirst = getFirstSegment(currentPath);
-  const urlFirst = getFirstSegment(new URL(url, window.location.origin).pathname);
-
-  const isActive = currentFirst === urlFirst;
-
   if (externalLink) {
     return (
       <a
         href={url}
-        className={`${styles.navLink} ${customClasses} ${isActive ? styles.isActive : ''}`}
-        {...rest}
+        className={`${styles.navLink} ${customClasses}`}
       >
         {children}
       </a>
     );
   }
+
     return (
       <NavLink
         to={url}
