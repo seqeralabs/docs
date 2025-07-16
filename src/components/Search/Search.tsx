40720afc1d0c47c8a63c4f023c6dea19fb3afdcd
dana.kim@seqera.io <dana.kim@seqera.io>
2025-07-16 14:38:57 -0400

    remove unused styles

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -59,14 +59,12 @@ export default function Search() {
 
       // Add styles to prevent scrolling on the body
       document.body.style.position = "fixed";
-      // document.body.style.top = `-${scrollY}px`;
       document.body.style.overflow = "hidden";
       document.body.style.width = "100%";
 
       return () => {
         // Re-enable scrolling when component unmounts or modal closes
         document.body.style.position = "";
-        document.body.style.top = "";
         document.body.style.overflow = "";
         document.body.style.width = "";
 
